795a2aebfc1f2c6eea652aa115df11ce96095507
Zack Zhang <zz2705@columbia.edu>
2019-12-05 04:47:15 +0000

    Add something new into the file

--- tools_squirrel/urls.py
+++ tools_squirrel/urls.py
@@ -19,5 +19,6 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('map/',include('map.urls')),
     path('sightings/',include('sightings.urls')),
 ]
