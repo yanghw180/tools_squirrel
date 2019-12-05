e0cdd69092b78a946408f0143c5f7390caf97f56
Zack Zhang <zz2705@columbia.edu>
2019-12-05 03:43:14 +0000

    Created for map
    This is the views.py file in map.

---
+++ map/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from sightings.models import Squirrel
+
+
+
+def default_map(request):
+    sightings = Squirrel.objects.all()
+
+    context = {'sightings':sightings}
+    return render(request,'map/map.html',context)
