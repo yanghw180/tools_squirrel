759d9b5af52b65c08fb22fbec48dbc4881322d77
Zack Zhang <zz2705@columbia.edu>
2019-12-05 04:44:55 +0000

    This is views.py file
    Used for map application(latitude and longitude)

--- map/views.py
+++ map/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
-from sightings.models import Squirrel
 from django.http import HttpResponse
-
-
-def default_map(request):
-    sightings = Squirrel.objects.all()
-
-    context = {'sightings':sightings}
-    return render(request,'map/map.html',context)
+from sightings.models import Squirrel
 
 def index(request):
-return HttpResponse(‘Hi!How are you’)
+    return HttpResponse('Hi!How are you')
 
+def map(request):
+    sightings = Squirrel.objects.all()
+    context = {'sightings': sightings}
+    return render(request, 'map/map.html', context)
+
+# Create your views here.
