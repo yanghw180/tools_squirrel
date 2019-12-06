f5dbe5cd04fa0e9e1c2d61c9943fab63ddc88d6b
Zack Zhang <zz2705@columbia.edu>
2019-12-06 06:54:15 +0000

    <!DOCTYPE html> <html lang="en">   <head>     <!-- Required meta tags -->     <meta charset="utf-8">     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
        <!-- Bootstrap CSS -->
        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
    
        <link rel="stylesheet"
              href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css"
              integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
              crossorigin=""/>
        <!-- Make sure you put this AFTER Leaflet's CSS -->
        <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js"
                integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og=="
                crossorigin=""></script>
    
        <style>
          html,body {
            height: 100%;
          }
        </style>
      </head>
      <body>
        <div id="map" class="container-fluid h-100"></div>
    
        <!-- Optional JavaScript -->
        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
    
        <!-- Leaflet JS -->
        <script>
            var map_center = [
                40.782091,  // Latitude
                -73.964285  // Longitude
            ];
            var zoom_level = 14;
            var map = L.map('map').setView(map_center, zoom_level);
            L.tileLayer('https://{server}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                server: 'a',  // can be 'a', 'b', or 'c'
                // must leave attribution in!
                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
            }).addTo(map);
            <!-- L.Icon.Default.prototype.options.iconSize = 15; -->
            <!-- L.Icon.Default.prototype.options.iconAnchor = [0, 0]; -->
            <!-- L.Icon.Default.prototype.options.shadowSize = 15; -->
    Putting csv into database

--- sightings/management/commands/import_squirrel_data.py
+++ sightings/management/commands/import_squirrel_data.py
@@ -1,32 +1,87 @@
 from django.core.management.base import BaseCommand, CommandError
 from sightings.models import Squirrel
 import csv
+import datetime
 
 # reference: https://docs.djangoproject.com/en/2.2/howto/custom-management-commands/
 class Command(BaseCommand):
-    help = 'A command that can be used to import the data from a given path'
+    help = 'import squirrel data from custom path'
+
+    def booler(self, b):
+        if b.lower() == "true":
+            return True
+        else:
+            return False
 
     def add_arguments(self, parser):
-        parser.add_argument('path',help='/path/to/file.csv',type=str)
+        parser.add_argument('path', type=str, help='/path/to/file.csv')
 
     def handle(self, *args, **options):
         path = options['path']
-        # reference: https://stackoverflow.com/questions/2459979/how-to-import-csv-data-into-django-models
+
         with open(path) as f:
-            reader = csv.reader(f)
-            first_row = 1
+            data = csv.reader(f)
 
-            for row in reader:
+            title = True
+            for row in data:
+                if title:
+                    title = False
+                else:
+                    if row[4] == 'AM':
+                        shift_temp = Squirrel.AM
+                    else:
+                        shift_temp = Squirrel.PM
 
-                if first_row == 1:
-                    first_row =0
-                    continue
+                    if row[7] == 'Adult':
+                        age_temp = Squirrel.ADULT
+                    elif row[7] == 'Juvenile':
+                        age_temp = Squirrel.JUVENILE
+                    else:
+                        age_temp = Squirrel.UNKNOWN
 
-                _, created = Squirrel.objects.get_or_create(
-                    longitude = float(row[0]),
-                    latitude = float(row[1]),
-                    squirrel_id = row[2],
-                )
+                    if row[8] == 'Gray':
+                        color_temp = Squirrel.GRAY
+                    elif row[8] == 'Cinnamon':
+                        color_temp = Squirrel.CINNAMON
+                    elif row[8] == 'Black':
+                        color_temp = Squirrel.BLACK
+                    else:
+                        color_temp = Squirrel.UNKNOWN
+
+                    if row[12] == 'Ground Plane':
+                        location_temp = Squirrel.GROUND_PLANE
+                    elif row[12] == 'Above Ground':
+                        location_temp = Squirrel.ABOVE_GROUND
+                    else:
+                        location_temp = Squirrel.UNKNOWN
+
+                    _, created = Squirrel.objects.get_or_create(
+                        longitude = float(row[0]),
+                        latitude = float(row[1]),
+                        squirrel_id = row[2],
+                        shift = shift_temp,
+                        date = datetime.date(int(row[5][-4:]),int(row[5][:2]),int(row[5][2:4])),
+                        age = age_temp,
+                        color = color_temp,
+                        location = location_temp,
+                        specific_location = row[14],
+                        running = self.booler(row[15]),
+                        chasing = self.booler(row[16]),
+                        climbing = self.booler(row[17]),
+                        eating = self.booler(row[18]),
+                        foraging = self.booler(row[19]),
+                        other_activities = row[20],
+                        kuks = self.booler(row[21]),
+                        quaas = self.booler(row[22]),
+                        moans = self.booler(row[23]),
+                        tail_flags = self.booler(row[24]),
+                        tail_twitches = self.booler(row[25]),
+                        approaches = self.booler(row[26]),
+                        indifferent = self.booler(row[27]),
+                        runs_from = self.booler(row[28]),
+                    )
+        # reference: https://stackoverflow.com/questions/2459979/how-to-import-csv-data-into-django-models
+
             # creates a tuple of the new object or
             # current object and a boolean of if it was created
 
